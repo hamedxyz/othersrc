da911cb7f213124df9e771d9d4ac26e7a614cdb9
AirKN <o2controltechpas@protonmail.com>
2022-10-09 19:08:26 +0100

    added sxiv and slock source files

---
+++ README.md
@@ -0,0 +1,2 @@
+# othersoft
+other suckless and non suckless source files for my setup
---
+++ slock/config.h
@@ -0,0 +1,12 @@
+/* user and group to drop privileges to */
+static const char *user  = "air";
+static const char *group = "air";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "#1d2021",     /* after initialization */
+	[INPUT] =  "#159278",   /* during input */
+	[FAILED] = "#ee9911",   /* wrong password */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
