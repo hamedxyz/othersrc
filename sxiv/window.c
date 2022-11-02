e37cd7b8b540704935d44440356da39a80dd6cda
AirKN <o2controltechpas@protonmail.com>
2022-11-02 18:05:02 +0100

    color sxiv

--- sxiv/window.c
+++ sxiv/window.c
@@ -119,7 +119,7 @@ void win_init(win_t *win)
 	f = win_res(db, RES_CLASS ".font", "monospace-8");
 	win_init_font(e, f);
 
-	bg = win_res(db, RES_CLASS ".background", "#1d2021");
+	bg = win_res(db, RES_CLASS ".background", "#000000");
 	fg = win_res(db, RES_CLASS ".foreground", "#fbf1c7");
 	win_alloc_color(e, bg, &win->bg);
 	win_alloc_color(e, fg, &win->fg);
