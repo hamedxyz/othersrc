4629e3558b16cb1d62ad1e0ffba743e5fb3af553
airkn <o2controltechpas@protonmail.com>
2022-12-11 11:32:19 +0100

    added screenpen

---
+++ screenpen/CMakeFiles/screenpen.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/screenpen.dir/src/main.cpp.o"
+  "CMakeFiles/screenpen.dir/src/main.cpp.o.d"
+  "CMakeFiles/screenpen.dir/src/moc_screenwidget.cpp.o"
+  "CMakeFiles/screenpen.dir/src/moc_screenwidget.cpp.o.d"
+  "CMakeFiles/screenpen.dir/src/screenwidget.cpp.o"
+  "CMakeFiles/screenpen.dir/src/screenwidget.cpp.o.d"
+  "screenpen"
+  "screenpen.pdb"
+  "src/moc_screenwidget.cpp"
+  "ui_screenwidget.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/screenpen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
