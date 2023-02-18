7a3b4017e0e926ec9838911e62523b74ac8c7cd3
airkn <o2controltechpas@protonmail.com>
2023-02-18 19:50:43 +0100

    fixed screenpen build n stuff

---
+++ screenpenbuild/CMakeFiles/screenpen.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/air/.local/src/othersrc/screenpen/src/main.cpp" "CMakeFiles/screenpen.dir/src/main.cpp.o" "gcc" "CMakeFiles/screenpen.dir/src/main.cpp.o.d"
+  "/home/air/.local/src/othersrc/screenpenbuild/src/moc_screenwidget.cpp" "CMakeFiles/screenpen.dir/src/moc_screenwidget.cpp.o" "gcc" "CMakeFiles/screenpen.dir/src/moc_screenwidget.cpp.o.d"
+  "/home/air/.local/src/othersrc/screenpen/src/screenwidget.cpp" "CMakeFiles/screenpen.dir/src/screenwidget.cpp.o" "gcc" "CMakeFiles/screenpen.dir/src/screenwidget.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
